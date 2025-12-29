9010511819255b40b69b720d4fba10c68e8f2716
vishwajeet <vishwajeet17raj@gmail.com>
2025-12-29 10:13:17 +0530

    Day7 Code Added

---
+++ VisualStudio/Lecture6/Indexer/MyData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexer
+{
+    public class MyData
+    {
+        private string[] values = new string[3];
+        
+        public string this[int index]
+        {
+            get
+            {
+                return values[index];
+            }
+            set
+            {
+                values[index] = value;
+            }
+        }
+
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            Employee obj = new Employee();
+            obj[0] = "c";
+            obj[1] = "c++";
+            obj[2] = "c#";
+
+            Console.WriteLine(obj[0]);
+            Console.WriteLine(obj[1]);
+            Console.WriteLine(obj[2]);
+        }
+    }
+}
