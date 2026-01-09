83032c92ebc9d18783f403da53932176d61614bb
vishwajeet <vishwajeet17raj@gmail.com>
2026-01-09 21:25:15 +0530

    Reflection Example

---
+++ VisualStudio/Lecture11/ReflectionExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace ReflectionExample
+{
+    public class Program
+    {
+        public class Department
+        {
+            public int Id { get; set; }
+            public string name { get; set; }
+
+            private string location;
+        }
+        public static void Main()
+        {
+            Department obj = new Department();
+
+            var props = obj.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance).ToList();
+
+            foreach (var prop in props)
+            {
+                Console.WriteLine(prop.Name);
+            }
+        }
+    }
+}
