985cd10dacc4d7454cf0d139aff9fc73d630c67e
vishwajeet <vishwajeet17raj@gmail.com>
2026-01-10 15:22:42 +0530

    Custom Exception (Day14)

---
+++ VisualStudio/Lecture13/CustomException/DivideByOddException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomException
+{
+    public class DivideByOddException : Exception
+    {
+        public DivideByOddException()
+        : base("Division by an odd number is not allowed.")
+        {
+        }
+    }
+    public class DivideByZeroInternalException : Exception
+    {
+        public DivideByZeroInternalException()
+        : base("Internal Exception Occurred. Please contact admin.")
+        {
+        }
+        public DivideByZeroInternalException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ VisualStudio/Lecture13/CustomException/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CustomException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numerator: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter denominator: ");
+            int b = int.Parse(Console.ReadLine());
+            try
+            {
+                int result = Divide(a, b);
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (DivideByOddException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch (DivideByZeroInternalException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        public static int Divide(int a, int b)
+        {
+            try
+            {
+                if (b % 2 == 1)
+                {
+                    throw new DivideByOddException();
+                }
+                return a / b;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw new DivideByZeroInternalException(
+                    "Internal Exception Occurred. Please contact admin.",
+                    ex
+                );
+            }
+        }
+    }
+}
