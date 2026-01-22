ef27903cc83d427a7cb1224c1f6f26c9fb0fd6a7
vishwajeet <vishwajeet17raj@gmail.com>
2026-01-22 15:50:44 +0530

    Attributes (Day23)

---
+++ VisualStudio/Lecture22/Attribute/AttributeExample/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AttributeExample
+{
+    public class Calculator
+    {
+        [Obsolete("Use the Add(int, int) method instead")]
+        public int OldAdd(int a ,int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a ,int b)
+        {
+            return a - b; 
+        }
+        public int Add(int a ,int b)
+        {
+            return a + b;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            int oldSum = c.OldAdd(5, 6);
+            int subtract = c.Subtract(5, 6);
+            int sum = c.Add(5, 6);
+            Console.WriteLine($"{oldSum}, {subtract}, {sum}");
+        }
+    }
+}
---
+++ VisualStudio/Lecture22/RefStruct/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RefStruct
+{
+    public ref struct TempBuffer
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Disposed");
+        }
+
+        public void DoWork()
+        {
+            Console.WriteLine("Working with buffer");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            UseBuffer();
+        }
+
+        static void UseBuffer()
+        {
+            using var buf = new TempBuffer();
+            buf.DoWork();
+        }
+    }
+}
