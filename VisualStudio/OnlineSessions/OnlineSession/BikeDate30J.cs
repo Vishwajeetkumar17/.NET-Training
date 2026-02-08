a6e441ca3f3d85631324f2a97873faffe926b970
vishwajeet <vishwajeet17raj@gmail.com>
2026-02-08 15:40:15 +0530

    Online Sessions Problems Solutions

---
+++ VisualStudio/OnlineSessions/OnlineSession/BikeDate30J.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// 30 Jan
+namespace OnlineSession
+{
+    public class BikeDate30J
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int PricePerDay { get; set; }
+    }
+
+    public class BikeUtility
+    {
+        public void AddBikeDetails(string model, string brand, int pricePerDay)
+        {
+            int key = Program.bikeDetails.Count + 1;
+            BikeDate30J bike = new BikeDate30J
+            {
+                Model = model,
+                Brand = brand,
+                PricePerDay = pricePerDay,
+            };
+            Program.bikeDetails.Add(key, bike);
+        }
+
+        public SortedDictionary<string, List<BikeDate30J>> GroupBikesByBrand()
+        {
+            SortedDictionary<string, List<BikeDate30J>> groupedBike = new SortedDictionary<string, List<BikeDate30J>>();
+            foreach (var item in Program.bikeDetails.Values)
+            {
+                if (!groupedBike.ContainsKey(item.Brand))
+                {
+                    groupedBike[item.Brand] = new List<BikeDate30J>();
+                }
+                groupedBike[item.Brand].Add(item);
+            }
+            return groupedBike;
+        }
+
+    }
+    public class Program
+    {
+        public static SortedDictionary<int, BikeDate30J> bikeDetails = new();
+        static void Main()
+        {
+            BikeUtility utility = new BikeUtility();
+            while (true)
+            {
+                Console.WriteLine("1. Add Bike Details");
+                Console.WriteLine("2. Group Bikes By Brand");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine();
+                Console.WriteLine("Enter your choice");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("Enter the model");
+                    string model = Console.ReadLine();
+                    Console.WriteLine("Enter the brand");
+                    string brand = Console.ReadLine();
+                    Console.WriteLine("Enter the price per day");
+                    int pricePerDay = int.Parse(Console.ReadLine());
+                    utility.AddBikeDetails(model, brand, pricePerDay);
+                    Console.WriteLine("Bike details added successfully");
+                }
+                else if (choice == 2)
+                {
+                    SortedDictionary<string, List<BikeDate30J>> groupedBikes = utility.GroupBikesByBrand();
+                    foreach (var item in groupedBikes)
+                    {
+                        Console.WriteLine(item.Key);
+                        foreach (var item2 in item.Value)
+                        {
+
+                            Console.WriteLine(item2.Model);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else if (choice == 3)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
