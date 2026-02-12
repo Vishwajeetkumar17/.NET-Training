03e48c7f38da8d51556e33704d3a11400ae102c5
vishwajeet <vishwajeet17raj@gmail.com>
2026-02-12 10:35:28 +0530

    Email Validation And Bank Transation (Day 37)

---
+++ VisualStudio/PracticeSessions/PracticeQuestionDay37/BankingInterestCalculation/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Transactions;
+using System.Xml.Linq;
+
+namespace BankingInterestCalculation
+{
+    public class BankAccount
+    {
+        public decimal Balance { get; set; }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            Console.WriteLine("Deposit Successful\nCurrent Balance: " + Balance);
+        }
+        public void WithDraw(decimal amount)
+        {
+            if(Balance > amount)
+            {
+                Balance -= amount;
+                Console.WriteLine("Withdraw Successful\nCurrent Balance:" + Balance);
+            }
+            else
+            {
+                Console.WriteLine("Amount is greater than Balance");
+                return;
+            }
+        }
+    }
+    public class SavingsAccount : BankAccount
+    {
+        public void CalcInterset(int interest)
+        {
+            Balance = Balance + (interest * Balance / 100);
+            Console.WriteLine("Balance after Interest: " + Balance);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Balance: ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+            BankAccount account = new BankAccount() { Balance = balance };
+            SavingsAccount savingAccount = new SavingsAccount() { Balance = balance };
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Interest");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine();
+                Console.WriteLine("Enter the Choice: ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        
+                        Console.WriteLine("Enter Amount to Deposit: ");
+                        decimal dAmount = decimal.Parse(Console.ReadLine());
+                        account.Deposit(dAmount);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter amount to withdraw");
+                        decimal wAmount = decimal.Parse(Console.ReadLine());
+                        account.WithDraw(wAmount);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the interest(%)");
+                        int interest = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Total Balance after Interest");
+                        savingAccount.CalcInterset(interest);
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ VisualStudio/PracticeSessions/PracticeQuestionDay37/EmailValidation/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace EmailValidation
+{
+    public class Program
+    {
+        public static bool EmailValidator(string message)
+        {
+            if(string.IsNullOrEmpty(message))
+            {
+                return false;
+            }
+            string lowerCheck = message.ToLower();
+            if(message != lowerCheck)
+            {
+                return false;
+            }
+            string[] check = message.Split("@");
+            if(check.Length != 2)
+            {
+                return false;
+            }
+            if (check[1] != "gmail.com")
+            {
+                return false;
+            }
+            List<char> invalidChars = new List<char>() { '_', '&', '=', '+', '-', '*', '/', '\'', ',', '<', '>', '!', ' ' };
+            foreach (var item in check[0])
+            {
+                foreach (char ch in invalidChars)
+                {
+                    if (ch == item)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (check.Contains(".."))
+            {
+                return false;
+            }
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a Gmail Id: ");
+                string input = Console.ReadLine();
+                string output = EmailValidator(input) ? "Valid" : "Invalid";
+                Console.WriteLine(output);
+                Console.WriteLine("Write 'quit' is terminate");
+                string exit = Console.ReadLine();
+                if (exit == "quit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
